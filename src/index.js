6d0da1ca9cfe71e4d23d0742954393bef20bbc3c
md shahjalal <shahjalal.freelancer@gmail.com>
2025-03-25 15:08:44 +0600

    stractuar ready

---
+++ src/index.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'custom-pink': '#FFD6D6',
+                        'custom-red': '#FF3B1D',
+                    }
+                }
+            }
+        }
